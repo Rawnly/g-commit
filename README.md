9698a984bca590eea46196ca05de0d1aec4af3c5
Rawnly <fedevitale99@gmail.com>
2017-06-13 22:25:30 +0200

    :tada:

---
+++ README.md
@@ -0,0 +1,35 @@
+# g-commit
+
+![placeholder][img_url]
+> Commit made easy
+
+## Installation
+To install **g-commit** you need 
+to use a **node package manager** as **npm** 
+or **yarn**
+
+```bash
+    $ npm install g-commit --global
+    
+    # or with shorthands
+    
+    $npm i -g g-commit 
+```
+
+## Usage
+```bash
+ $ commit --help
+   Commit made easy
+
+Usage: committer <selector> <commit> [-a]
+```
+
+## Tested Platforms
+  - [x] macOs
+  - [x] Windows
+  - [x] Linux Systems
+  
+## Credits
+File generated with [readme generator](https://rawnly.com/projects/rmd-gen/) by [@Rawnly](https://rawnly.com).
+
+[img_url]: https://placehold.it/1920x1080?text=g-commit+Screenshot
---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/local/bin node
+const fs = require('fs');
+
+const Meow = require('meow');
+const execa = require('execa');
+const chalk = require('chalk');
+
+const cli = new Meow(`
+
+    Usage: ${chalk.yellow('committer')} ${chalk.gray('<selector> <commit> [-a]')}
+    \n
+
+`, {
+	alias: {
+		a: 'all',
+		h: 'help',
+		v: 'version'
+	}
+});
+
+const committer = function (action, flags) {
+	const commit = (action[1]) ? action[1] : ':tada:';
+	const selector = action[0] ? action[0] : '*';
+
+	if (fs.existsSync('./.git') === false) {
+		execa('git', ['init']).then(result => {
+			console.log(result.stdout);
+			console.log();
+			console.log();
+			execa('git', ['add', selector]).then(() => {
+				if (flags.add) {
+					execa('git', ['commit', '-a', '-m', commit]).then(result => {
+						console.log(result.stdout);
+					}).catch(e => {
+						console.log(e.message);
+					});
+				} else {
+					execa('git', ['commit', '-m', commit]).then(result => {
+						console.log(result.stdout);
+					}).catch(e => {
+						console.log(e.message);
+					});
+				}
+			}).catch(e => {
+				console.log(e.message);
+			});
+		}).catch(e => {
+			console.log(e.message);
+		});
+	} else {
+		execa('git', ['add', selector]).then(() => {
+			if (flags.add) {
+				execa('git', ['commit', '-a', '-m', commit]).then(result => {
+					console.log(result.stdout);
+				}).catch(e => {
+					console.log(e.message);
+				});
+			} else {
+				execa('git', ['commit', '-m', commit]).then(result => {
+					console.log(result.stdout);
+				}).catch(e => {
+					console.log(e.message);
+				});
+			}
+		}).catch(e => {
+			console.log(e.message);
+		});
+	}
+};
+
+committer(cli.input, cli.flags);
